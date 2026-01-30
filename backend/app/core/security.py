b3d4fb4d478bd8823bd28befc96c6bb2351f5921
Wiktoria Pilch <wiktoria.pilch01@gmail.com>
2026-01-31 00:55:24 +0100

    Security update
    functions distribution for JWT and password.

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,8 +1,30 @@
 from datetime import datetime, timedelta, timezone
-from typing import Any, Union
+from typing import Any
 from jose import jwt
 from passlib.context import CryptContext
 from app.core.config import settings
 
-ALGORITHM = "256"
+ALGORITHM = "HS256"
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def create_token(subject: str|int, expires_delta: timedelta, type: str):
+    expire = datetime.now(timezone.utc) + expires_delta
+    to_encode = {
+        "exp": expire,
+        "sub": str(subject),
+        "type": type
+    }
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def create_access_token():
+    None
+
+def create_refresh_token():
+    None
+
+def get_password_hash():
+    None
+
+def verify_password():
+    None
