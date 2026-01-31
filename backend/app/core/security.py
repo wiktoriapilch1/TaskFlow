1262cf5cf7b8e97b238509790a7fe6a85ed5ca71
Wiktoria Pilch <wiktoria.pilch01@gmail.com>
2026-01-31 01:04:51 +0100

    Added logic to security functions

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -7,7 +7,7 @@ from app.core.config import settings
 ALGORITHM = "HS256"
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def create_token(subject: str|int, expires_delta: timedelta, type: str):
+def _create_token(subject: str|int, expires_delta: timedelta, type: str):
     expire = datetime.now(timezone.utc) + expires_delta
     to_encode = {
         "exp": expire,
@@ -17,14 +17,16 @@ def create_token(subject: str|int, expires_delta: timedelta, type: str):
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-def create_access_token():
-    None
+def create_access_token(subject: str|int) -> str:
+    expires_delta = timedelta(minutes=1)
+    return _create_token(subject, expires_delta, type="access")
 
-def create_refresh_token():
-    None
+def create_refresh_token(subject: str|int) -> str:
+    expires_delta = timedelta(days=7)
+    return _create_token(subject, expires_delta, type="refresh")
 
-def get_password_hash():
-    None
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password) 
 
-def verify_password():
-    None
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
