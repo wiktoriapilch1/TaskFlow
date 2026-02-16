23a3310e33598ce552f1f7392f34a2dab20172c9
Wiktoria Pilch <wiktoria.pilch01@gmail.com>
2026-02-16 16:11:50 +0100

    PostgreSQL and Alembic Configuration

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,9 +1,7 @@
 from sqlmodel import create_engine, Session
 from app.core.config import settings
 
-connect_args = {"check_same_thread": False}
-
-engine = create_engine(settings.DATABASE_URL, echo=True, connect_args=connect_args)
+engine = create_engine(settings.DATABASE_URL, echo=True)
 
 def get_session():
     with Session(engine) as session:
